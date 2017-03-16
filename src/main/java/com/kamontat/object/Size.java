394b4b892200eda4e74290eaa337c1d1d55a309f
kamontat <kamontat_c@hotmail.com>
2017-03-16 16:16:37 +0700

    set size

--- src/main/java/com/kamontat/object/Size.java
+++ src/main/java/com/kamontat/object/Size.java
@@ -3,23 +3,25 @@ package com.kamontat.object;
 import com.kamontat.constants.SizeUnit;
 import com.kamontat.constants.SizeUnitType;
 
+import java.math.BigDecimal;
+
 /**
  * @author kamontat
  * @version 1.0
  * @since Thu 16/Mar/2017 - 12:22 AM
  */
 public class Size {
-	private long size;
+	private BigDecimal size;
 	private SizeUnit unit;
 	private SizeUnitType type;
 	
-	public Size(long size, SizeUnit unit, SizeUnitType type) {
+	public Size(BigDecimal size, SizeUnit unit, SizeUnitType type) {
 		this.size = size;
 		this.unit = unit;
 		this.type = type;
 	}
 	
-	public long getSize() {
+	public BigDecimal getSize() {
 		return size;
 	}
 	
@@ -29,5 +31,10 @@ public class Size {
 	
 	public SizeUnitType getType() {
 		return type;
+	}
+	
+	public Size convertTo(SizeUnit unit) {
+		BigDecimal size = this.unit.to(this.size, unit);
+		return new Size(size, unit, type);
 	}
 }
