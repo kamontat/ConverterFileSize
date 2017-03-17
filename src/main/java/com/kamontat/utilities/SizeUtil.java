474daf6acefc40c0cca2d6e53e94b7243b6ad358
kamontat <kamontat_c@hotmail.com>
2017-03-17 21:32:17 +0700

    clean and fixed scale of number is 0

--- src/main/java/com/kamontat/utilities/SizeUtil.java
+++ src/main/java/com/kamontat/utilities/SizeUtil.java
@@ -6,12 +6,16 @@ import com.kamontat.object.Size;
 
 import java.math.BigDecimal;
 
+import static com.kamontat.object.Size.DEFAULT_ROUNDING_MODE;
+import static com.kamontat.object.Size.SIZE_SCALE;
+
 /**
  * @author kamontat
  * @version 1.0
  * @since Wed 15/Mar/2017 - 5:41 PM
  */
 public class SizeUtil {
+	
 	/**
 	 * change byte to minimum size <br>
 	 * from: <a href="http://stackoverflow.com/questions/3758606/how-to-convert-byte-size-into-human-readable-format-in-java">stackoverflow</a>
@@ -20,18 +24,18 @@ public class SizeUtil {
 	 * 		number of bytes
 	 * @param si
 	 * 		is si unit
-	 * @return string format %.1f %s
+	 * @return string format %.1f
 	 */
 	public static String humanReadableByteCount(long bytes, boolean si) {
 		if (bytes < 0) throw new IllegalArgumentException("input bytes must be positive number.");
 		int unit = si ? 1000: 1024;
-		if (bytes < unit) return bytes + " B";
+		if (bytes < unit) return bytes + " BYTE";
 		int exp = (int) (Math.log(bytes) / Math.log(unit));
 		String pre = (si ? "kMGTPE": "KMGTPE").charAt(exp - 1) + (si ? "": "i");
 		return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
 	}
 	
 	public static Size getSize(long size, SizeUnit unit, SizeUnitType type) {
-		return new Size(BigDecimal.valueOf(size), unit, type);
+		return new Size(BigDecimal.valueOf(size).setScale(SIZE_SCALE, DEFAULT_ROUNDING_MODE), unit, type);
 	}
 }
