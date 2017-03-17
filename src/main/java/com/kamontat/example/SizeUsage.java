072fa4dc9af60a456b479b68eb3b5ebc450ca26b
kamontat <kamontat_c@hotmail.com>
2017-03-17 21:33:53 +0700

    [Size] fixed bug, clean the code and do a lot of thing

--- src/main/java/com/kamontat/example/SizeUsage.java
+++ src/main/java/com/kamontat/example/SizeUsage.java
@@ -1,9 +1,21 @@
 package com.kamontat.example;
 
+import com.kamontat.constants.SizeUnitType;
+import com.kamontat.object.Size;
+import com.kamontat.utilities.SizeUtil;
+
+import static com.kamontat.constants.SizeUnit.BYTE;
+import static com.kamontat.constants.SizeUnit.EB;
+
 /**
  * @author kamontat
  * @version 1.0
  * @since Thu 16/Mar/2017 - 4:04 PM
  */
 public class SizeUsage {
+	public static void main(String[] args) {
+		Size DEFAULT = SizeUtil.getSize(10, EB, SizeUnitType.NON_SI);
+		Size s = DEFAULT.convertTo(BYTE).convertTo(SizeUnitType.SI).convertTo(EB).convertTo(SizeUnitType.SI);
+		System.out.println(s);
+	}
 }
--- src/main/java/com/kamontat/object/Size.java
+++ src/main/java/com/kamontat/object/Size.java
@@ -4,21 +4,42 @@ import com.kamontat.constants.SizeUnit;
 import com.kamontat.constants.SizeUnitType;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 /**
+ * size of file, supported both si and non-si format
+ *
  * @author kamontat
  * @version 1.0
  * @since Thu 16/Mar/2017 - 12:22 AM
  */
 public class Size {
+	/**
+	 * default scale of BigDecimal size
+	 */
+	public static int SIZE_SCALE = 99;
+	/**
+	 * default of rounding the number
+	 */
+	public static RoundingMode DEFAULT_ROUNDING_MODE = RoundingMode.HALF_EVEN;
+	
+	/**
+	 * size supported until 2^Integer.MAX_VALUE
+	 */
 	private BigDecimal size;
+	/**
+	 * size unit (as <b>Kilobyte</b>, <b>Megabyte</b>, etc.) <br>
+	 * and inside unit already have type of it
+	 *
+	 * @see SizeUnit
+	 */
 	private SizeUnit unit;
-	private SizeUnitType type;
 	
 	public Size(BigDecimal size, SizeUnit unit, SizeUnitType type) {
+		if (size.scale() == 0) size = size.setScale(SIZE_SCALE, DEFAULT_ROUNDING_MODE);
 		this.size = size;
-		this.unit = unit;
-		this.type = type;
+		this.unit = unit.setType(type);
 	}
 	
 	public BigDecimal getSize() {
@@ -30,11 +51,55 @@ public class Size {
 	}
 	
 	public SizeUnitType getType() {
-		return type;
+		return unit.getType();
 	}
 	
+	/**
+	 * convert current unit to newUnit
+	 *
+	 * @param unit
+	 * 		the new unit
+	 * 		convert to unit {@link SizeUnit}
+	 * @return {@link Size}
+	 */
 	public Size convertTo(SizeUnit unit) {
-		BigDecimal size = this.unit.to(this.size, unit);
-		return new Size(size, unit, type);
+		this.size = this.unit.to(this.size, unit);
+		this.unit = unit;
+		return this;
+	}
+	
+	/**
+	 * convert current type to newType
+	 *
+	 * @param type
+	 * 		the new type
+	 * @return {@link Size}
+	 */
+	public Size convertTo(SizeUnitType type) {
+		this.size = size.multiply(BigDecimal.valueOf(type.getSize())).divide(BigDecimal.valueOf(getType().getSize()), size.scale(), RoundingMode.HALF_EVEN);
+		this.unit.setType(type);
+		return this;
+	}
+	
+	/**
+	 * get size in string format
+	 *
+	 * @param decimalNumber
+	 * 		number that come after dot(.)
+	 * @return string of size (example: 45233.1234 ~~ 45,233.12 ,if parameter is 2)
+	 */
+	public String getSizeAsString(int decimalNumber) {
+		String pattern = "#,##0.";
+		for (int i = 0; i < decimalNumber; i++) {
+			pattern += "0";
+		}
+		DecimalFormat df = new DecimalFormat(pattern);
+		df.setRoundingMode(DEFAULT_ROUNDING_MODE);
+		return df.format(size);
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("type=(%s) %.6f %s", getType(), size, unit.getString());
 	}
 }
