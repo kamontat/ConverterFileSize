db80fc95f9b991243ce711a701770dee6e5a3f8f
kamontat <kamontat_c@hotmail.com>
2017-03-20 01:53:43 +0700

    add zip and unzip

--- src/main/java/com/kamontat/utilities/ZipsUtil.java
+++ src/main/java/com/kamontat/utilities/ZipsUtil.java
@@ -1,10 +1,10 @@
 package com.kamontat.utilities;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 /**
  * @author kamontat
@@ -17,16 +17,13 @@ public class ZipsUtil {
 	// read buffer
 	private static byte[] buffer = new byte[SIZE];
 	
-	/**
-	 * Unzip it
-	 *
-	 * @param zipFile
-	 * 		input zip file
-	 * @param outputFolder
-	 * 		zip file output folder
-	 */
 	public static void unZip(String zipFile, String outputFolder) {
+		unZip(zipFile, outputFolder, pathname -> true);
+	}
+	
+	public static void unZip(String zipFile, String outputFolder, FileFilter filter) {
 		cleanBuffer();
+		
 		try {
 			//create output directory is not exists
 			File folder = new File(outputFolder);
@@ -40,13 +37,12 @@ public class ZipsUtil {
 				File file = new File(outputFolder + File.separator + fileName);
 				if (ze.isDirectory()) {
 					FilesUtil.createFolders(file.getAbsolutePath());
-				} else {
+				} else if (filter.accept(file)) {
 					StringBuilder sb = new StringBuilder();
 					int len;
 					while ((len = zis.read(buffer)) > 0) {
 						sb.append(new String(buffer, 0, len));
 					}
-					
 					System.out.println("unzip file: " + file.getAbsoluteFile());
 					FilesUtil.newFile(file.getAbsolutePath(), sb.toString());
 				}
@@ -60,8 +56,39 @@ public class ZipsUtil {
 		}
 	}
 	
-	
-	
+	// TODO 3/20/2017 AD 1:53 AM NO TEST YET
+	public void zip(String zipFile, String inputFolder) {
+		cleanBuffer();
+		
+		try {
+			FileOutputStream fos = new FileOutputStream(zipFile);
+			ZipOutputStream zos = new ZipOutputStream(fos);
+			
+			System.out.println("Output to Zip : " + zipFile);
+			
+			List<String> all = FilesUtil.getAllFileNames(inputFolder, true);
+			
+			for (String file : all) {
+				
+				System.out.println("File Added : " + file);
+				ZipEntry ze = new ZipEntry(file);
+				zos.putNextEntry(ze);
+				
+				FileInputStream in = new FileInputStream(inputFolder + File.separator + file);
+				
+				int len;
+				while ((len = in.read(buffer)) > 0) {
+					zos.write(buffer, 0, len);
+				}
+				
+				in.close();
+			}
+			zos.closeEntry();
+			zos.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
 	
 	private static void cleanBuffer() {
 		buffer = new byte[SIZE];
